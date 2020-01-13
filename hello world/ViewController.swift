581b67b770fff086f737c43c9404614cb399d1b8
connor <connorkeenan5@icloud.com>
2020-01-13 12:21:13 -0600

    Initial Commit

---
+++ hello world/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  hello world
+//
+//  Created by Connor Keenan on 9/3/19.
+//  Copyright © 2019 Connor Keenan. All rights reserved.
+//
+
+import UIKit
+
+@IBOutlet class ViewController:
+    
+weak var favoriteBand: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var redButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
